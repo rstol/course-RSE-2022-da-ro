e574bc800009c58707f288b8e6a021e44cc3bca3
David Bohner <dbohner@student.ethz.ch>
2022-05-19 10:50:30 +0200

    Added more testcases to narrow down source of Unreachable segment validations

--- analysis/src/test/java/ch/ethz/rse/integration/tests/Unreachable_Test.java
+++ analysis/src/test/java/ch/ethz/rse/integration/tests/Unreachable_Test.java
@@ -9,15 +9,13 @@ import ch.ethz.rse.Event;
 
 public class Unreachable_Test {
     public static void m2(int i) {
-        Event e = new Event(0, 100);
-        if (i >= 10 && i < 100) {
-            e = new Event(10, 100);
+        Event e = new Event(42, 128);
+        if (i >= 10) {
             if (i < 10) {
-                //unreachable
-                e.switchLights(-2);
+                //unreachable, would violate AFTER_START if not
+                Event u = new Event(128, 42);
+                e.switchLights(2);
             }
-            e.switchLights(i);
         }
-        e.switchLights(42);
     }
 }
